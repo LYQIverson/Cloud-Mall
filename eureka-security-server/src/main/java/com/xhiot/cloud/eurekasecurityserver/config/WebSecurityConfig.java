a01b5ab3497ec11c028efde9522d93c3e2218777
liuyq <2568835102@qq.com>
2020-12-31 16:36:44 +0800

    first commit

---
+++ eureka-security-server/src/main/java/com/xhiot/cloud/eurekasecurityserver/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.xhiot.cloud.eurekasecurityserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @ClassName WebSecurityConfig
+ * Create by Liuyq on 2020/12/2 14:23
+ **/
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+
+}
---
+++ eureka-security-server/src/test/java/com/xhiot/cloud/eurekasecurityserver/EurekaSecurityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhiot.cloud.eurekasecurityserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSecurityServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
